5006318d54b3234cc31488a164838c6102ac0ff0
Ryota Ikezawa <pavegy@gmail.com>
2020-11-23 08:49:43 +0900

    :shirt: Fix lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import Link from 'next/link';
-import Layout from '../components/layout';
-import { Article } from '../types/articles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import { Article } from '../types/articles';
+import Layout from '../components/layout';
 import ButtonLink from '../components/button_link';
 
 type Props = {
@@ -14,33 +13,31 @@ type Props = {
 
 const formatDate = (date: Date) => `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
 
-const Index: React.FC<Props> = ({ articles }) => {
-  return (
-    <Layout>
-      <Box m={2}>
-        <Grid container spacing={1}>
-          {articles.map((article: Article) => {
-            const date = new Date(article.date);
-            return (
-              <Grid key={article.id} item xs={6} sm={4}>
-                <Paper variant="outlined">
-                  <Box p={1}>
-                    <Typography variant="caption">{formatDate(date)}</Typography>
-                    <Box textAlign="center">
-                      <ButtonLink small href={`articles/${article.id}`}>
-                        {article.title}
-                      </ButtonLink>
-                    </Box>
+const Index: React.FC<Props> = ({ articles }) => (
+  <Layout>
+    <Box m={2}>
+      <Grid container spacing={1}>
+        {articles.map((article: Article) => {
+          const date = new Date(article.date);
+          return (
+            <Grid key={article.id} item xs={6} sm={4}>
+              <Paper variant="outlined">
+                <Box p={1}>
+                  <Typography variant="caption">{formatDate(date)}</Typography>
+                  <Box textAlign="center">
+                    <ButtonLink small href={`articles/${article.id}`}>
+                      {article.title}
+                    </ButtonLink>
                   </Box>
-                </Paper>
-              </Grid>
-            );
-          })}
-        </Grid>
-      </Box>
-    </Layout>
-  );
-};
+                </Box>
+              </Paper>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
+  </Layout>
+);
 
 export const getStaticProps = async () => {
   const res = await fetch('https://samples.microcms.io/api/v1/articles/', {
